93991483d6c41f34c95da3c82860b48062903ee7
Joyce Perez <japerez.ph@gmail.com>
2018-10-02 21:19:44 +0800

    code along, activity 1 and 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,192 @@
-// EXTERNAL JS
+// MONDAY & TUES CODE ALONG
+// Hidden for Act 1 and 2 to work
 
 
-function external_function(){
-	confirm("This is external");
-	alert("Thank you");
+
+// document.getElementById('changeColorToBlue').onclick = function() {
+// 	document.getElementById('heading').style.color = "blue";
+// }
+
+// document.getElementById('changeContent').onclick = function() {
+// 	document.getElementById('content').innerHTML = "Welcome to JS!";
+// }
+
+// document.getElementById('changeContent').ondblclick = function() {
+// 	document.getElementById('content').innerHTML = "Bye Felicia!";
+// }
+
+// document.getElementById('changeHeight').onclick = function() {
+// 	document.getElementById('box').style.height = "200px";
+// }
+
+// document.getElementById('changeImage').onclick = function() {
+// 	document.getElementById('img').src = "assets/images/2.jpg";
+// }
+
+// document.getElementById('imahe').onclick = function() {
+// 	document.getElementById('newImage').src = "assets/images/1.jpg";
+// }
+
+// document.getElementById('heading').onclick = function() {
+// 	document.getElementById('heading').innerHTML = "Hello!";
+// }
+
+
+
+
+
+// ACTIVITY 1
+// Hidden so Act 2 will work
+
+// document.getElementById('hideAll').onclick = function() {
+// 	document.getElementById('container').style.display = "none";
+// }
+
+
+// document.getElementById('showAll').onclick = function() {
+// 	document.getElementById('container').style.display = "block";
+// }
+
+// document.getElementById('changeHtmlContent').onclick = function() {
+// 	document.getElementById('statementTwo').innerHTML = "See? It changed!";
+// }
+
+
+// document.getElementById('changeMyHeight').onclick = function() {
+// 	document.getElementById('img1').style.height = "100px";
+// }
+
+// document.getElementById('changeMyImage').onclick = function() {
+// 	document.getElementById('img1').src ="assets/images/2.jpg"
+// }
+
+// document.getElementById('changeMyStyle').onclick = function() {
+// 	document.getElementById('statementFour').style.color = "green";
+// }
+
+
+
+
+
+
+
+
+
+// ACTIVITY 2
+// Hide to view Act 1
+
+
+document.getElementById('btnRed').onclick = function() {
+	document.getElementById('red').style.background = "red";
 }
+
+document.getElementById('btnRed').ondblclick = function() {
+	document.getElementById('red').style.background = "none";
+}
+
+
+
+
+document.getElementById('btnOrange').onclick = function() {
+	document.getElementById('orange').style.background = "orange";
+}
+
+document.getElementById('btnOrange').ondblclick = function() {
+	document.getElementById('orange').style.background = "none";
+}
+
+
+
+
+document.getElementById('btnYellow').onclick = function() {
+	document.getElementById('yellow').style.background = "yellow";
+}
+
+document.getElementById('btnYellow').ondblclick = function() {
+	document.getElementById('yellow').style.background = "none";
+}
+
+
+
+
+
+document.getElementById('btnGreen').onclick = function() {
+	document.getElementById('green').style.background = "green";
+}
+
+document.getElementById('btnGreen').ondblclick = function() {
+	document.getElementById('green').style.background = "none";
+}
+
+
+
+
+document.getElementById('btnBlue').onclick = function() {
+	document.getElementById('blue').style.background = "blue";
+}
+
+document.getElementById('btnBlue').ondblclick = function() {
+	document.getElementById('blue').style.background = "none";
+}
+
+
+
+
+document.getElementById('btnIndigo').onclick = function() {
+	document.getElementById('indigo').style.background = "#4b0082";
+}
+
+document.getElementById('btnIndigo').ondblclick = function() {
+	document.getElementById('indigo').style.background = "none";
+}
+
+
+// VIOLET ALL
+document.getElementById('btnViolet').onclick = function() {
+	document.getElementById('red').style.background ="#EE82EE";
+	document.getElementById('orange').style.background ="#EE82EE";
+	document.getElementById('yellow').style.background ="#EE82EE";
+	document.getElementById('green').style.background ="#EE82EE";
+	document.getElementById('blue').style.background ="#EE82EE";
+	document.getElementById('indigo').style.background ="#EE82EE";
+}
+
+document.getElementById('btnViolet').ondblclick = function() {
+	document.getElementById('red').innerHTML = "OMG!";
+	document.getElementById('orange').innerHTML = "JAVASCRIPT";
+	document.getElementById('yellow').innerHTML = "IS";
+	document.getElementById('green').innerHTML = "SOOOOO";
+	document.getElementById('blue').innerHTML = "AMAZING";
+	document.getElementById('indigo').innerHTML = "& FUN!!!";
+}
+
+
+// CLEAR
+document.getElementById('btnClear').onclick = function () {
+	document.getElementById('red').style.background = "none";
+	document.getElementById('orange').style.background = "none";
+	document.getElementById('yellow').style.background = "none";
+	document.getElementById('green').style.background = "none";
+	document.getElementById('blue').style.background = "none";
+	document.getElementById('indigo').style.background = "none";
+}
+
+
+
+document.getElementById('btnClear').ondblclick = function () {
+	document.getElementById('red').innerHTML = " ";
+	document.getElementById('orange').innerHTML = " ";
+	document.getElementById('yellow').innerHTML = " ";
+	document.getElementById('green').innerHTML = " ";
+	document.getElementById('blue').innerHTML = " ";
+	document.getElementById('indigo').innerHTML = " ";
+
+
+}
+
+
+
+
+
+
+
